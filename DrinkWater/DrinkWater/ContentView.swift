8182915689f7f7c08c481c6ab7f2f79c72833598
gaeng2y <gaeng2y@gmail.com>
2023-06-24 14:06:21 +0900

    2023 06 24 init

---
+++ DrinkWater/DrinkWater/ContentView.swift
@@ -0,0 +1,41 @@
+//
+//  ContentView.swift
+//  DrinkWater
+//
+//  Created by Kyeongmo Yang on 2023/06/24.
+//
+
+import SwiftUI
+import WidgetKit
+
+struct ContentView: View {
+    @State private var counter = 0
+    
+    var body: some View {
+        VStack {
+            Image("waterbottle")
+                .resizable()
+                .frame(
+                    width: 200,
+                    height: 200,
+                    alignment: .center
+                )
+            Text("\(counter)잔")
+                .font(.largeTitle)
+            Button(action: {
+                self.counter += 1
+                WidgetCenter.shared.reloadTimelines(ofKind: "Example2Widget")
+            }) {
+                Text("Increase Counter")
+                    .padding()
+                    .background(Color.blue)
+                    .foregroundColor(.white)
+                    .cornerRadius(10)
+            }
+        }
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ DrinkWater/DrinkWater/DrinkWaterApp.swift
@@ -0,0 +1,17 @@
+//
+//  DrinkWaterApp.swift
+//  DrinkWater
+//
+//  Created by Kyeongmo Yang on 2023/06/24.
+//
+
+import SwiftUI
+
+@main
+struct DrinkWaterApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
