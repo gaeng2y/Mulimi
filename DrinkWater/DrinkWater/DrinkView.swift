012293f2a76dca527a426fbbbb9e522075d92a33
gaeng2y <gaeng2y@gmail.com>
2023-06-29 23:16:43 +0900

    애니메이션 고치는 중

--- DrinkWater/DrinkWater/DrinkView.swift
+++ DrinkWater/DrinkWater/DrinkView.swift
@@ -12,7 +12,7 @@ struct DrinkView: View {
     @State private var counter = 0
     @State private var isPresented: Bool = false
     @State var progress: CGFloat = 0
-    @State var startAnimation: CGFloat = 0.5
+    @State var startAnimation: CGFloat = 0
     
     var body: some View {
         ZStack {
@@ -21,64 +21,64 @@ struct DrinkView: View {
             
             VStack {
                 // MARK: 물방울
-                ZStack {
-                    GeometryReader { proxy in
-                        let size = proxy.size
+            ZStack {
+                GeometryReader { proxy in
+                    let size = proxy.size
+                    
+                    ZStack {
+                        Image(systemName: "drop.fill")
+                            .resizable()
+                            .renderingMode(.template)
+                            .aspectRatio(contentMode: .fit)
+                            .foregroundColor(.white)
+                            .scaleEffect(x: 1.1, y: 1.1)
+                            .offset(y: -1)
                         
-                        ZStack {
-                            Image(systemName: "drop.fill")
-                                .resizable()
-                                .renderingMode(.template)
-                                .aspectRatio(contentMode: .fit)
-                                .foregroundColor(.white)
-                                .scaleEffect(x: 1.1, y: 1.1)
-                                .offset(y: -1)
-                            
-                            WaterWave(progress: self.progress, waveHeight: 0.1, offset: self.startAnimation)
-                                .fill(.teal)
-                            // water drop
-                                .overlay {
-                                    ZStack {
-                                        Circle()
-                                            .fill(.white.opacity(0.1))
-                                            .frame(width: 15, height: 15)
-                                            .offset(x: -20)
-                                        
-                                        Circle()
-                                            .fill(.white.opacity(0.1))
-                                            .frame(width: 15, height: 15)
-                                            .offset(x: 40, y: 30)
-                                        
-                                        Circle()
-                                            .fill(.white.opacity(0.1))
-                                            .frame(width: 25, height: 25)
-                                            .offset(x: -30, y: 80)
-                                        
-                                        Circle()
-                                            .fill(.white.opacity(0.1))
-                                            .frame(width: 25, height: 25)
-                                            .offset(x: 50, y: 70)
-                                        
-                                        Circle()
-                                            .fill(.white.opacity(0.1))
-                                            .frame(width: 10, height: 10)
-                                            .offset(x: 40, y: 100)
-                                    }
+                        WaterWave(progress: self.progress, waveHeight: 0.1, offset: self.startAnimation)
+                            .fill(.teal)
+                        // water drop
+                            .overlay {
+                                ZStack {
+                                    Circle()
+                                        .fill(.white.opacity(0.1))
+                                        .frame(width: 15, height: 15)
+                                        .offset(x: -20)
+                                    
+                                    Circle()
+                                        .fill(.white.opacity(0.1))
+                                        .frame(width: 15, height: 15)
+                                        .offset(x: 40, y: 30)
+                                    
+                                    Circle()
+                                        .fill(.white.opacity(0.1))
+                                        .frame(width: 25, height: 25)
+                                        .offset(x: -30, y: 80)
+                                    
+                                    Circle()
+                                        .fill(.white.opacity(0.1))
+                                        .frame(width: 25, height: 25)
+                                        .offset(x: 50, y: 70)
+                                    
+                                    Circle()
+                                        .fill(.white.opacity(0.1))
+                                        .frame(width: 10, height: 10)
+                                        .offset(x: 40, y: 100)
                                 }
-                                .mask {
-                                    Image(systemName: "drop.fill")
-                                        .resizable()
-                                        .aspectRatio(contentMode: .fit)
-                                }
-                        }
-                        .frame(width: size.width, height: size.height, alignment: .center)
-                        .onAppear {
-                            withAnimation(.linear(duration: 2).repeatForever(autoreverses: false)) {
-                                startAnimation = size.width
                             }
+                            .mask {
+                                Image(systemName: "drop.fill")
+                                    .resizable()
+                                    .aspectRatio(contentMode: .fit)
+                            }
+                    }
+                    .frame(width: size.width, height: size.height, alignment: .center)
+                    .onAppear {
+                        withAnimation(.linear(duration: 2).repeatForever(autoreverses: false)) {
+                            startAnimation = size.width
                         }
                     }
-                    .frame(height: 350)
+                }
+                .frame(height: 450)
                 }
                 .padding(EdgeInsets(top: 0, leading: 0, bottom: 10, trailing: 0))
             
@@ -101,6 +101,10 @@ struct DrinkView: View {
                         }
                         UserDefaults.shared.set(self.counter, forKey: key)
                         WidgetCenter.shared.reloadTimelines(ofKind: "DrinkWaterWidget")
+                        
+                        withAnimation(.linear(duration: 2).repeatForever(autoreverses: false)) {
+                            startAnimation = 2
+                        }
                     }) {
                         Text("마시기")
                             .padding()
@@ -140,7 +144,7 @@ struct DrinkView: View {
 
 struct DrinkView_Previews: PreviewProvider {
     static var previews: some View {
-        DrinkView(progress: 0.5, startAnimation: 0.5)
+        DrinkView(progress: 0.5, startAnimation: 0)
     }
 }
 
@@ -152,7 +156,7 @@ struct WaterWave: Shape {
     var offset: CGFloat
     
     // Enabling Animation
-    var animateableDAta: CGFloat {
+    var animateableData: CGFloat {
         get { offset }
         set { offset = newValue }
     }
