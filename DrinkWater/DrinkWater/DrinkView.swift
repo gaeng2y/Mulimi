b32eeed4b4b7899802312add31845c02939084f6
gaeng2y <gaeng2y@gmail.com>
2023-07-12 20:26:50 +0900

    애니메이션 startAnimation 값 수정

--- DrinkWater/DrinkWater/DrinkView.swift
+++ DrinkWater/DrinkWater/DrinkView.swift
@@ -86,8 +86,8 @@ struct DrinkView: View {
                     }
                     .frame(width: size.width, height: size.height, alignment: .center)
                     .onAppear {
-                        withAnimation(.linear(duration: 2.0).repeatForever(autoreverses: false)) {
-                            startAnimation = size.width
+                        withAnimation(.linear(duration: 2).repeatForever(autoreverses: false)) {
+                            startAnimation = size.width * 0.91094147585
                         }
                     }
                 }
