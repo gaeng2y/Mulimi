56f9c440fd9781da2e1f5d2dc17b3585a350954d
gaeng2y <gaeng2y@gmail.com>
2023-08-03 23:58:55 +0900

    OnboardingView 추가

---
+++ DrinkWater/DrinkWater/OnboardingVIew.swift
@@ -0,0 +1,20 @@
+//
+//  OnboardingVIew.swift
+//  DrinkWater
+//
+//  Created by Kyeongmo Yang on 2023/08/03.
+//
+
+import SwiftUI
+
+struct OnboardingVIew: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct OnboardingVIew_Previews: PreviewProvider {
+    static var previews: some View {
+        OnboardingVIew()
+    }
+}
