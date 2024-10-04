40729ca3c799b5a87047de0138573cc97b9ae8fc
gaeng2y <gaeng2y@gmail.com>
2024-10-04 21:11:33 +0900

    Splash 관련 코드 제거

--- App/Sources/App/Root.swift
+++ App/Sources/App/Root.swift
@@ -11,34 +11,16 @@ import ComposableArchitecture
 struct Root {
     @ObservableState
     struct State {
-        var isFirstLaunching = true
         var drinkWater = DrinkWater.State()
     }
     
-    @Dependency(\.continuousClock) var clock
-    private enum CancelID { case timer }
-    
     enum Action {
-        case onAppear
-        case timerTicked
         case drinkWater(DrinkWater.Action)
     }
     
     var body: some ReducerOf<Self> {
         Reduce { state, action in
             switch action {
-            case .onAppear:
-                return .run { send in
-                    for await _ in self.clock.timer(interval: .seconds(2)) {
-                        await send(.timerTicked, animation: .interpolatingSpring(stiffness: 3000, damping: 40))
-                    }
-                }
-                .cancellable(id: CancelID.timer, cancelInFlight: true)
-                
-            case .timerTicked:
-                state.isFirstLaunching = false
-                return .cancel(id: CancelID.timer)
-                
             case .drinkWater:
                 return .none
             }
